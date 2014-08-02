069b43e3ed54d911c8e3c8c3637a8296f1a045fd
Yougansh <youganshsharma@gmail.com>
2014-08-03 03:30:20 +0530

    Thread Example Handling Exception

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,30 +1,30 @@
-class ThreadTest{
-public static void main(String s[]) throws InterruptedException
+class ThreadTest
 {
-Runnable r = new Runnable(){
+	public static void main(String s[]) throws InterruptedException
+	{
+		Runnable r = new Runnable()
+		{
 
-public void run()
- {
-  try
-  {
-      Thread.sleep(1000);
-  }
-  catch(InterruptedException e)
-  {
-      System.out.println("interrupted");
-  }
-System.out.println("ran");
- }
-};
+			public void run()
+ 			{
+  			try
+  			{
+ 				Thread.sleep(1000);
+ 			}
+  			catch(InterruptedException e)
+  			{
+				System.out.println("interrupted");
+			}
+				System.out.println("ran");
+			}
+		};
 
-
-Thread t = new Thread(r);
-  t.start();
-System.out.println("started");
-  t.sleep(200);
-System.out.println("interrupting");
-  t.interrupt();
-System.out.println("ended");
-
-}
+	Thread t = new Thread(r);
+  	t.start();
+	System.out.println("started");
+	t.sleep(200);
+	System.out.println("interrupting");
+	t.interrupt();
+	System.out.println("ended");
+	}
 }
